20b517f2897056faa09baf408d4d2e4572249121
CaptainObjective <marcinwos18@gmail.com>
2020-04-18 02:11:22 +0200

    refactoring

--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -1,21 +1,24 @@
 class SearchBar extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = this.render();
+    this.render();
+
     this.input = this.querySelector('input');
     this.icon = this.querySelector('i');
-
     this.icon.addEventListener('click', () => this.handleSubmit());
   }
+
   handleSubmit() {
     const event = new CustomEvent('searching-images', { detail: this.input.value });
     this.dispatchEvent(event);
   }
+
   render() {
-    return `<div class="ui big icon input">
-                <input type="text" placeholder="Search big...">
-                <i class="search icon link"></i>
-            </div>`;
+    this.innerHTML = `
+    <div class="ui big icon input">
+        <input type="text" placeholder="Search big...">
+        <i class="search icon link"></i>
+    </div>`;
   }
 }
 
