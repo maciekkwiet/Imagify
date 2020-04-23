98656d54302b68adc674c1f9b95d06b1cd165ca3
Juniper <agata5810@gmail.com>
2020-04-23 22:28:53 +0200

    komponent działa <3

--- client/src/Components/Image.js
+++ client/src/Components/Image.js
@@ -5,21 +5,20 @@ class Image extends HTMLElement {
     constructor() {
       super();
       this.image = [];
-      this.render();
     }
     //const basic = Observable.create
     connectedCallback() {
-    
+      this.render();
     }
     
-    createImage1() {
+    createImage() {
         return `<img class="Image-url" src="${"https://66.media.tumblr.com/9ff6c8d5478fe6ea666d6f14f5342c1b/tumblr_mevhkec7OG1qfvx4yo1_500.gifv"}"/>`;
       }
 
     render() {
         this.innerHTML = `
-        <div class="image-url">
-        ${this.createImage1()}
+        <div class="app-image">
+        ${this.createImage()}
         </div>`;
       }
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import SearchBar from './Components/SearchBar.js';
 import ImageList from './Components/ImageList.js';
+import Image from './Components/Image.js';
 
 const setup = () => {
   customElements.define('search-bar', SearchBar);
   customElements.define('image-list', ImageList);
+  customElements.define('app-image', Image);
 };
 
 window.addEventListener('load', setup);
