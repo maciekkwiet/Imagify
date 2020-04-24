6696ccf45edf6107baff01ad218cc6303326b091
kacperjanusz <kaacper.janusz@gmail.com>
2020-04-24 18:22:52 +0200

    3 API's implemented, with results shuffling

--- client/src/Api/ImageService.js
+++ client/src/Api/ImageService.js
@@ -1,13 +1,51 @@
 import { UNSPLASH_CLIENT_ID } from '../../secrets';
+import { PIXABAY_KEY } from '../../secrets';
+import { PEXELS_KEY } from '../../secrets';
 
 class ImageService {
-  constructor() {}
+  constructor() {
+    this.allImages = [];
+    this.unsplashImages;
+    this.pixabayImages;
+    this.pexelsImages;
+    this.pixmacImages;
+    this.isUnspalshChecked = true;
+    this.isPixabayChecked = true;
+    this.isPexelsChecked = true;
+  }
   async getImages(searchText) {
-    const response = await fetch(
-      `https://api.unsplash.com/search/photos?page=1&query=${searchText}&client_id=${UNSPLASH_CLIENT_ID}`,
-    );
-    const { results } = await response.json();
-    return results.map((image) => image.urls.small);
+    let response;
+    if (this.isUnspalshChecked) {
+      response = await fetch(
+        `https://api.unsplash.com/search/photos?page=1&query=${searchText}&client_id=${UNSPLASH_CLIENT_ID}`,
+      );
+      const { results } = await response.json();
+      this.unsplashImages = results.map((image) => image.urls.small);
+    }
+    if (this.isPixabayChecked) {
+      response = await fetch(`https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${searchText}&image_type=photo`);
+      const { hits } = await response.json();
+      this.pixabayImages = hits.map((image) => image.webformatURL);
+    }
+    if (this.isPexelsChecked) {
+      response = await fetch(`https://api.pexels.com/v1/search?query=${searchText}&per_page=80&page=1`, {
+        headers: {
+          Authorization: PEXELS_KEY,
+        },
+      });
+      const { photos } = await response.json();
+      this.pexelsImages = photos.map((image) => image.src.medium);
+    }
+    return this.shuffleImages(this.allImages.concat(this.unsplashImages, this.pixabayImages, this.pexelsImages));
+  }
+  shuffleImages(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
   }
 }
 
