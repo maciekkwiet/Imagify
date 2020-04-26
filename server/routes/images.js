7b646a0d057dae9bd90692aaff499f09394b1776
CaptainObjective <marcinwos18@gmail.com>
2020-04-27 00:14:28 +0200

    all services

--- server/routes/images.js
+++ server/routes/images.js
@@ -5,10 +5,21 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const { searchText } = req.query;
-  const { data: unsplash } = await axios.get(
+  const unsplashRequest = axios.get(
     `https://api.unsplash.com/search/photos?page=1&query=${searchText}&client_id=${process.env.UNSPLASH_CLIENT_ID}`,
   );
-  res.json({ unsplash });
+  const pixabayRequest = axios.get(
+    `https://pixabay.com/api/?key=${process.env.PIXABAY_KEY}&q=${searchText}&image_type=photo`,
+  );
+  const pexelsRequest = axios.get(`https://api.pexels.com/v1/search?query=${searchText}&per_page=80&page=1`, {
+    headers: {
+      Authorization: process.env.PEXELS_KEY,
+    },
+  });
+
+  const responses = await Promise.all([unsplashRequest, pixabayRequest, pexelsRequest]);
+  const [unsplash, pixabay, pexels] = responses.map(({ data }) => data);
+  res.json({ unsplash, pixabay, pexels });
 });
 
 module.exports = router;
