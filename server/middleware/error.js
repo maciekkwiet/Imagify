00daafddb767aea36f9fa0a52da2720784c15e9d
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-04-26 23:56:42 +0200

    add next

--- server/middleware/error.js
+++ server/middleware/error.js
@@ -1,6 +1,4 @@
-module.exports = function (err, req, res,next) {
-    console.log(err);
-    res.status(500).json( {error : 'Something failed'} )
-    next();
-  };
-  
+module.exports = function (err, req, res, next) {
+  console.log(err);
+  res.status(500).json({ error: 'Something failed' });
+};
