4e3fed10c8845a50eeff1bffeaa3f6c33367616c
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-05-01 17:45:45 +0200

    new path

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,17 +1,18 @@
 const jwt = require('jsonwebtoken');
 const jwtKey = process.env.JWT_SECRET;
-const { User } = require('../model/user');
+// const { User } = require('../model/user');
 
 module.exports = function (req, res, next) {
   const token = req.header('x-auth');
+
   if (!token) return res.status(401).json({ error: 'No token' });
 
   try {
     const decodedUser = jwt.verify(token, jwtKey);
     req.user = decodedUser;
-    const { _id } = req.user;
-    let user = User.findOne({ _id });
-    next(user);
+    // const { _id } = req.user;
+    // let user = User.findOne({ _id });
+    next( );
   } catch (ex) {
     console.log(ex);
     res.status(400).json({ error: 'Invalid token' });
--- server/routes/favourities.js
+++ server/routes/favourities.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const router = express.Router();
-// const {user} = require('../middleware/auth')
+const { user } = require('../middleware/auth');
 const { User } = require('../model/user');
+// const auth = require('../middleware/auth');
 
 router.get('/', async (req, res) => {
   const { _id } = req.user;
   let user = await User.findOne({ _id });
+  console.log(req.user);
   const { favourities } = user;
   res.send({ favourities });
+  console.log(favourities);
 });
 
 router.post('/:url', async (req, res) => {
   const { url } = req.params;
   const { _id } = req.user;
   let user = await User.findOne({ _id });
-
   const { favourities } = user;
 
   if (!favourities.includes(url)) {
@@ -29,7 +31,7 @@ router.delete('/:url', async (req, res) => {
   const { _id } = req.user;
   let user = await User.findOne({ _id });
   const { favourities } = user;
-  const newarray = favourities.filter((x) => x !== url);
+  const newarray = favourities.filter((el) => el !== url);
   res.json({ favourities: newarray });
 });
 
