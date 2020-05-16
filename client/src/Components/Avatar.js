029e83748db02d5986af2e9e3954c01e18948867
kacperjanusz <kaacper.janusz@gmail.com>
2020-05-16 16:40:50 +0200

    request try

--- client/src/Components/Avatar.js
+++ client/src/Components/Avatar.js
@@ -1,4 +1,4 @@
-import store from '../Store';
+import axios from 'axios';
 
 class Avatar extends HTMLElement {
   connectedCallback() {
@@ -6,21 +6,50 @@ class Avatar extends HTMLElement {
     this.avatar = document.createElement('app-avatar');
     console.log('dupsko');
     this.renderAvatarPlace();
-    this.renderUpload();
+    this.renderUploadWindow();
 
     this.chooseBox = document.querySelector('.choose-box');
     this.chooseBox.appendChild(this.avatarPlace);
     this.input = document.querySelector('input[type = "file"]');
     const attachButton = document.querySelector('.attachButton').addEventListener('click', () => {
+      console.log(localStorage.getItem('token'));
+      this.uploadAvatar();
+
       console.log('dupa2');
-      console.log(this.input.files[0]);
-      //this.renderAvatar();
+      console.log(this.file);
     });
     console.log(document.querySelector('.attachButton'));
 
     console.log('dupa3');
   }
-  renderUpload() {
+  async uploadAvatar() {
+    this.token = localStorage.getItem('token');
+    this.file = this.input.files[0];
+    let config = {
+      headers: {
+        token: this.token,
+      },
+    };
+
+    let data = {
+      file: this.file,
+    };
+
+    //axios.post(URL, data, config).then(...)
+
+    try {
+      const response = await axios.post('api/upload-avatar', data);
+      console.log(response);
+
+      //this.closeModal();
+    } catch (ex) {
+      console.error(ex);
+      $('body').toast({
+        message: ex.response.data.error,
+      });
+    }
+  }
+  renderUploadWindow() {
     this.innerHTML = `
     
     <i class="close icon"></i>
