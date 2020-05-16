1ea7ca9a146103743181114d43e134af1d1f5880
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-05-16 14:02:00 +0200

    auth changes

--- server/routes/avatar.js
+++ server/routes/avatar.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const cloudinary = require('cloudinary').v2;
-const cloudinarySecret = process.env.CLAUDINARY_SECRET;
-const cloudinaryKey = process.env.CLAUDINARY_KEY;
-const cloudinaryName = process.env.CLOUDINARY_NAME;
+const api_secret = process.env.CLAUDINARY_SECRET;
+const api_key = process.env.CLAUDINARY_KEY;
+const cloud_name = process.env.CLOUDINARY_NAME;
 
 cloudinary.config({
-  cloud_name: 'dixekqpwi',
-  api_key: '698369834765699',
-  api_secret: '-bXjadjyGbOMFYKFP3aZOhOTtU4',
+  cloud_name: cloud_name,
+  api_key: api_key,
+  api_secret: api_secret,
 });
 
 router.post('/', async (req, res) => {
   const file = req.files.photo;
-  console.log(req.user.token);
-  console.log(file);
+  // console.log(req.user.token);
+  // console.log(file);
   const result = await cloudinary.uploader.upload(file.tempFilePath);
-  console.log(result);
-  console.log('Result', result.url);
+  // console.log(result);
+  // console.log('Result', result.url);
   req.user.avatar = result.url;
   const { avatar } = await req.user.save();
   res.json({ avatar });
