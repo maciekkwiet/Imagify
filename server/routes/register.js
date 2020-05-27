d8e8d975b3f2b309cd159e553546807d226574ad
KlaraGajaszek <klara.gajaszek@student.po.edu.pl>
2020-05-28 00:26:07 +0200

    for merge

--- server/routes/register.js
+++ server/routes/register.js
@@ -10,27 +10,25 @@ const Welcome = require('./email/emailitems');
 const jwtKey = process.env.JWT_SECRET;
 
 router.post('/', async (req, res) => {
-
   const { error } = User.validate(req.body);
   if (error) return res.status(400).json({ error: error.details[0].message });
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).json({ error: 'User already registered' });
   else {
-    user = await new User(_.pick(req.body, ['email', 'password', 'favourities','avatar']));
+    user = await new User(_.pick(req.body, ['email', 'password', 'favourities', 'avatar']));
   }
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
   const token = jwt.sign({ _id: user._id }, jwtKey);
 
-
   // wysyłam do klienta tylko wyselekcjonowane dane
   //auth => name of header
   //token =>value
-  res.header('auth', token).send(_.pick(user, ['email', 'favourities']));
-
+  res.header('x-auth', token).send(_.pick(user, ['email', 'favourities']));
 
   const itemsWelcom = await Welcome.welcome(user.email);
   await sendWelcomeEmail(user.email, itemsWelcom.subject, itemsWelcom.text, itemsWelcom.html);
+  console.log(token);
 });
 module.exports = router;
