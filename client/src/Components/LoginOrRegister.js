16b287bfc1d8fa60b9d0bb1e2fc5e1b249151904
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-05-31 13:03:19 +0200

    switch

--- client/src/Components/LoginOrRegister.js
+++ client/src/Components/LoginOrRegister.js
@@ -1,54 +1,54 @@
 class LoginOrRegister extends HTMLElement {
   constructor() {
     super();
-    this.isLoginOpen = true;
-    this.isForgotten = false;
-    this.switchDisplay = this.switchDisplay.bind(this);
+    this.currentWindow;
+    this.LOGIN = true;
+    this.currentWindow = 'LOGIN';
+    this.loginDisplay = this.loginDisplay.bind(this);
     this.forgotDisplay = this.forgotDisplay.bind(this);
-    this.switchLabel = document.createElement('span');
+    this.loginLabel = document.createElement('span');
     this.forgotLabel = document.createElement('span');
-    this.switchLabel.addEventListener('click', this.switchDisplay);
+    this.loginLabel.addEventListener('click', this.loginDisplay);
     this.forgotLabel.addEventListener('click', this.forgotDisplay);
   }
 
   connectedCallback() {
     this.render();
-    this.appendChild(this.switchLabel);
   }
 
   forgotDisplay() {
-    this.isForgotten = !this.isForgotten; // this.isLoginOpen = !this.isLoginOpen;
+    this.currentWindow = 'FORGOTTEN';
     this.render();
   }
 
-  switchDisplay() {
-    this.isLoginOpen = !this.isLoginOpen;
+  loginDisplay() {
+    this.LOGIN = !this.LOGIN;
+    if (this.LOGIN) {
+      this.currentWindow = 'LOGIN';
+    } else {
+      this.currentWindow = 'REGISTER';
+    }
     this.render();
   }
 
   render() {
-    this.innerHTML = `
-    ${
-      this.isLoginOpen
-        ? this.isForgotten
-          ? '<app-resetpassword>dupa</app-resetpassword>'
-          : '<app-loginForm></app-loginForm>'
-        : '<app-registrationform></app-registrationform>'
+    switch (this.currentWindow) {
+      case 'LOGIN':
+        this.innerHTML = '<app-loginForm></app-loginForm>';
+        this.appendChild(this.loginLabel);
+        this.loginLabel.innerHTML = `Don't have account yet? Click here to sign up.`;
+        this.querySelector('.labelPassword').appendChild(this.forgotLabel);
+        this.forgotLabel.innerHTML = `Forgot a password? Click here to reset`;
+        break;
+      case 'REGISTER':
+        this.innerHTML = '<app-registrationform></app-registrationform>';
+        this.appendChild(this.loginLabel);
+        this.loginLabel.innerHTML = `Already have an account? Click here to sign in.`;
+        break;
+      case 'FORGOTTEN':
+        this.innerHTML = '<app-resetpassword></app-resetpassword>';
+        break;
     }
-    `;
-    this.switchLabel.innerHTML = this.isForgotten
-      ? ''
-      : this.isLoginOpen
-      ? `Don't have account yet? Click here to sign up.`
-      : `Already have an account? Click here to sign in.`;
-    this.appendChild(this.switchLabel);
-    this.forgotLabel.innerHTML = this.isForgotten
-      ? ''
-      : this.isLoginOpen
-      ? `Forgot a password? Click here to reset`
-      : ``;
-    this.labelPassword = this.querySelector('.labelPassword');
-    this.isLoginOpen ? (!this.isForgotten ? this.labelPassword.appendChild(this.forgotLabel) : null) : null;
   }
 }
 
