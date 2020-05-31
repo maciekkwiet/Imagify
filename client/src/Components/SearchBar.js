bccd408381310aa7f2edbc79aeaa5473889482a7
wbrydak <brydakwojciech@gmail.com>
2020-05-31 17:16:23 +0200

    fix

--- client/src/Components/SearchBar.js
+++ client/src/Components/SearchBar.js
@@ -3,7 +3,7 @@ import store from '../Store';
 class SearchBar extends HTMLElement {
   connectedCallback() {
     this.render();
-    this.input = this.querySelector('input');
+    this.input = this.querySelector('.input-legit');
     store.searchTextInput = fromEvent(this.input, 'input');
   }
 
@@ -11,7 +11,7 @@ class SearchBar extends HTMLElement {
     this.innerHTML = `
     <div class="ui big icon input four wide column searchBarMain">
       <input style="display:none" type="text" name="fakeusernameremembered"/>
-      <input type="text" autocomplete="chrome-off" placeholder="Search big...">
+      <input class="input-legit" type="text" autocomplete="chrome-off" placeholder="Search big...">
       <i class="search icon"></i>
     </div>`;
   }
