eb42577e8e3970f80109f90101a04dcbf5e8fbac
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-06-01 23:24:15 +0200

    style filter by orientation

--- client/src/Components/SortAndFilter.js
+++ client/src/Components/SortAndFilter.js
@@ -9,56 +9,26 @@ class SortAndFilter extends HTMLElement {
 
   render() {
     this.innerHTML = `
-<div class="ui grid">
+<div class="ui grid filt">
       <div class="ui floating dropdown labeled icon button">
         <i class="filter icon"></i>
-        <span class="text">Filter by Color</span>
+        <span class="text">Filter by Orientation</span>
         <div class="menu">
             <div class="ui icon search input">
                 <i class="search icon"></i>
-                <input type="text" placeholder="Search color...">
+                <input type="text" placeholder="Search orientation...">
             </div>
             <div class="divider"></div>
             <div class="header">
                 <i class="tags icon"></i>
-                Tag color
+                Tag orientation
             </div>
             <div class="scrolling menu">
             <div class="item">
-                <div class="ui red empty circular label"></div>
-                Red
+                <i class="arrows alternate horizontal icon"></i> Horizontal
             </div>
             <div class="item">
-                <div class="ui blue empty circular label"></div>
-                Blue
-            </div>
-            <div class="item">
-                <div class="ui black empty circular label"></div>
-                Black
-            </div>
-            <div class="item">
-                <div class="ui purple empty circular label"></div>
-                Purple
-            </div>
-            <div class="item">
-                <div class="ui orange empty circular label"></div>
-                Orange
-            </div>
-            <div class="item">
-                <div class="ui white empty circular label"></div>
-                White
-            </div>
-            <div class="item">
-                <div class="ui yellow empty circular label"></div>
-                Yellow
-            </div>
-            <div class="item">
-                <div class="ui pink empty circular label"></div>
-                Pink
-            </div>
-            <div class="item">
-                <div class="ui green empty circular label"></div>
-                Green
+                <i class="arrows alternate vertical icon"></i> Vertical
             </div>
         </div>
         </div>
@@ -79,9 +49,12 @@ class SortAndFilter extends HTMLElement {
             </div>
             <div class="scrolling menu">
             <div class="item">
-                <div class="ui red empty circular label"></div>
-                Red
+                Any color
             </div>
+            <div class="item">
+            <div class="ui red empty circular label"></div>
+            Red
+        </div>
             <div class="item">
                 <div class="ui black empty circular label"></div>
                 Black
