6015e279a8009f0e4440e104bc92634274c85127
Maciej Kwietniewski <maciek.kwiet36@gmail.com>
2020-06-04 19:42:51 +0200

    2/2

--- server/routes/images.js
+++ server/routes/images.js
@@ -10,29 +10,21 @@ const schema = Joi.object({
     .items(Joi.string().uppercase().valid('UNSPLASH', 'PIXABAY', 'PEXELS'))
     .unique()
     .default(['UNSPLASH', 'PIXABAY', 'PEXELS']),
-  color: Joi.string(),
-  orderBy: Joi.string(),
-  orientation: Joi.string(),
-  filters: Joi.object(),
+
+  orderBy: Joi.string().valid('min_popularity', 'max_popularity'),
+  filters: {
+    orientation: Joi.string().valid('Vertical', 'Horizontal'),
+    color: Joi.string().valid('red', 'black', 'white', 'yellow', 'orange', 'green', 'blue'),
+  },
 });
 
 router.post('/', async (req, res) => {
-  console.log(req.body);
-
   const { searchText } = req.query;
+  const { orderBy } = req.body;
   const {
-    filters: { color, orientation, orderBy },
+    filters: { color, orientation },
   } = req.body;
-
-  console.log(color);
-  console.log(orientation);
-  console.log(orderBy);
-
   let apiRequests = [];
-  let orderByUs;
-  let orderByPb;
-  let orientationUs;
-  let orientationPb;
 
   const {
     value: { services },
@@ -41,41 +33,13 @@ router.post('/', async (req, res) => {
 
   if (error) return res.status(400).json({ error: error.details[0].message });
 
-  const newArrayColor = services.filter((serviceName) => serviceName !== 'PEXELS');
-
-  if (color) {
+  if (color || orientation || orderBy) {
+    const newArrayColor = services.filter((serviceName) => serviceName !== 'PEXELS');
     apiRequests = newArrayColor.map((serviceName) =>
-      createRequest(serviceName, searchText, color, orderByUs, orderByPb, orientationUs, orientationPb),
+      createRequest(serviceName, searchText, color, orderBy, orientation),
     );
-  }
-
-  if (orderBy == 'min_popularity') {
-    orderByUs = 'latest';
-    orderByPb = 'latest';
-    apiRequests = newArrayColor.map((serviceName) =>
-      createRequest(serviceName, searchText, color, orderByUs, orderByPb, orientationUs, orientationPb),
-    );
-  }
-  if (orderBy == 'max_popularity') {
-    orderByUs = 'relevant';
-    orderByPb = 'popular';
-    apiRequests = newArrayColor.map((serviceName) =>
-      createRequest(serviceName, searchText, color, orderByUs, orderByPb, orientationUs, orientationPb),
-    );
-  }
-  if (orientation == 'Vertical') {
-    orientationUs = 'landscape';
-    orientationPb = 'vertical';
-    apiRequests = newArrayColor.map((serviceName) =>
-      createRequest(serviceName, searchText, color, orderByUs, orderByPb, orientationUs, orientationPb),
-    );
-  }
-  if (orientation == 'Horizontal') {
-    orientationUs = 'portrait';
-    orientationPb = 'horizontal';
-    apiRequests = newArrayColor.map((serviceName) =>
-      createRequest(serviceName, searchText, color, orderByUs, orderByPb, orientationUs, orientationPb),
-    );
+  } else {
+    apiRequests = services.map((serviceName) => createRequest(serviceName));
   }
 
   const responses = await Promise.all(apiRequests);
